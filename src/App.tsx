3008e404cc80f344b4e6b6874d1aba407de81525
raulbarossi <raulb1007@gmail.com>
2023-07-22 20:36:05 -0300

    atts

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,17 @@ import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 
+import Database from "tauri-plugin-sql-api";
+
+// sqlite. The path is relative to `tauri::api::path::BaseDirectory::App`.
+const db = await Database.load("sqlite:test.db");
+// mysql
+// const db = await Database.load("mysql://user:pass@host/database");
+// postgres
+// const db = await Database.load("postgres://postgres:password@localhost/test");
+
+await db.execute("INSERT INTO ...");
+
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
