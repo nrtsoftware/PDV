2d5165a3bc9aadd25d08c2bd8726fa7b57225739
Nivs <nivsoficial@gmail.com>
2023-07-23 14:48:31 -0300

    feat/fix: atts about data persistence

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -10,56 +10,56 @@
 struct Person {
     id: i32,
     name: String,
-    data: Option<Vec<u8>>,
+    password: String,
+    // data: Option<Vec<u8>>,
 }
 
 
 use rusqlite::{Connection, Result};
 use serde_json::json;
 
-
 #[tauri::command]
 async fn database_test() -> Result<String, String> {
     // Err("This failed!");
     let conn: Connection = Connection::open("./app.sqlite3").map_err(|err| err.to_string())?;
     // Imprimir a string JSON resultante
-    conn.execute(
-        "CREATE TABLE person (
-            id   INTEGER PRIMARY KEY,
-            name TEXT NOT NULL,
-            data BLOB
-        )",
-        (),
-    ).map_err(|err| err.to_string())?;
+    // conn.execute(
+    //     "CREATE TABLE person (
+    //         id   INTEGER PRIMARY KEY,
+    //         name TEXT NOT NULL,
+    //         password TEXT NOT NULL,
+    //     )",
+    //     (),
+    // ).map_err(|err| err.to_string())?;
 
     //Some other queries and operations if needed
-    let me = Person {
-        id: 0,
-        name: "Nivaldinho".to_string(),
-        data: None,
-    };
+    // let me = Person {
+    //     id: 0,
+    //     name: "Nivaldinho".to_string(),
+    //     data: None,
+    // };
 
-    let me2 = Person {
-        id: 0,
-        name: "barosso".to_string(),
-        data: None,
-    };
-    conn.execute(
-        "INSERT INTO person (name, data) VALUES (?1, ?2)",
-        (&me.name, &me.data),
-    ).map_err(|err| err.to_string())?;
+    // let me2 = Person {
+    //     id: 0,
+    //     name: "barosso".to_string(),
+    //     data: None,
+    // };
+    // conn.execute(
+    //     "INSERT INTO person (name, data) VALUES (?1, ?2)",
+    //     (&me.name, &me.data),
+    // ).map_err(|err| err.to_string())?;
 
-    conn.execute(
-        "INSERT INTO person (name, data) VALUES (?1, ?2)",
-        (&me2.name, &me2.data),
-    ).map_err(|err| err.to_string())?;
+    // conn.execute(
+    //     "INSERT INTO person (name, data) VALUES (?1, ?2)",
+    //     (&me2.name, &me2.data),
+    // ).map_err(|err| err.to_string())?;
 
-    let mut stmt = conn.prepare("SELECT id, name, data FROM person").map_err(|err| err.to_string())?;
+    let mut stmt = conn.prepare("SELECT id, name, password FROM person").map_err(|err| err.to_string())?;
     let person_iter = stmt.query_map([], |row| {
         Ok(Person {
             id: row.get(0)?,
             name: row.get(1)?,
-            data: row.get(2)?,
+            password: row.get(2)?,
         })
     }).map_err(|err| err.to_string())?;
     
@@ -71,7 +71,7 @@ async fn database_test() -> Result<String, String> {
         let person_json = json!({
             "id": person.id,
             "name": person.name,
-            "data": person.data
+            "password": person.password
         });
         data.push(person_json);
     }
